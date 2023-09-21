3e11075e7e32c7052408d66a7a1a81508137e33d
Susanta Goswami <susnitdgp@gmail.com>
2023-09-21 05:49:44 +0000

    Notification

--- popup.js
+++ popup.js
@@ -3,6 +3,25 @@ function myAlert(){
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("This is a popup!")
-    document.getElementById('sbmt').addEventListener('click', myAlert);
+    console.log("This is a popup!");
+
+    //submit button click event listener
+    const sbmt = document.getElementById('sbmt');
+    sbmt.addEventListener('click', myAlert);
+
+    const basic = document.getElementById('basic');
+    const progressNotif = document.getElementById('progress');
+    const list = document.getElementById('list');
+
+    basic.addEventListener('click', () => {
+        let options = {
+          type: 'basic',
+          title: 'Basic Notification',
+          message: 'This is a Basic Notification',
+          iconUrl: 'images/get_started16.png'
+        };
+        chrome.notifications.create(options);
+      });
+
+
 });
