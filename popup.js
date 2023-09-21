821d3ed9cc97b825b73443742771b865124de681
Susanta Goswami <susnitdgp@gmail.com>
2023-09-21 03:31:36 +0000

    popup js added

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
 console.log("This is a popup!");
+
+function myAlert(){
+    alert('Button Clicked');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+   document.getElementById('sbmt').addEventListener('click', myAlert);
+});
